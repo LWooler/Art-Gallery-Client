7739b25fe695f31748416b07eccbb5f76a6053ef
lwooler <lwooler@att.net>
2022-06-27 01:18:48 -0500

    refactored tests

--- src/app/Components/home/home.component.spec.ts
+++ src/app/Components/home/home.component.spec.ts
@@ -20,4 +20,17 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should have as title 'Art-Gallery-Client'`, () => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('Art-Gallery-Client');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Art-Gallery-Client app is running!');
+  });
 });
